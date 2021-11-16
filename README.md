b072c4c730f488cb11e6a3ab5f4831e2b370c10e
Andrey Sokoloff <smalldirtybird@gmail.com>
2021-11-16 14:39:53 +0300

    upload to telegram channel all downloaded photo

--- README.md
+++ README.md
@@ -3,14 +3,17 @@
 Easy way to download some beautiful pictures from SpaceX and NASA
 
 ## How to prepare:
-1. Make shore Python installed on your PC - you can get it from [official website](https://www.python.org/)
+1. Make sure Python installed on your PC - you can get it from [official website](https://www.python.org/)
 2. Install libraries with pip:
 `pip3 install -r requirements.txt`
 3. Get your personal token for access to NASA API on [NASA website](https://api.nasa.gov/).
 Create .env file in directory with main.py file(use Notepad++) and add the string `NASA_TOKEN='your_nasa_api_token'` to it (without any quotes).
 4. Create a Telegram bot which will post pictures to your own channel - just send message `/newbot` to [@BotFather](https://telegram.me/BotFather) and follow instructions.
 After bot will be created, get token from @BotFather and add to .env file like `TELEGRAM_TOKEN='your_telegram_bot_token'`.
-5. Create your Telegram channel (or use existing) and make your bot admin in it.
+5. Create your Telegram channel (or use existing) and make your bot admin in it. The add a string like `TELEGRAM_CHAT_ID=@YourChannelID` to .env file.
+6. At least, add to .env file string like `DELAY='time_of_delay_in_seconds`. 
+Instead of value in quotes indicate time when script should be started again in seconds.
+Default delay value is 24 hours.
 
 ## How to use:
 Run `main.py` with console. Use `cd` command if you need to change directory:
@@ -25,7 +28,7 @@ Get available options with `-h` argument:
 D:\learning\python\api_services\space_photo_loader>python main.py -h
 usage: main.py [-h] [-l LAUNCH_NUMBER] [-c COUNT] [-d DAYS_AGO] [-dir DIRECTORY]
 
-Загрузите красивые фото космоса от SpaceX и NASA
+Загрузка фото космоса от SpaceX и NASA в Телеграм-канал
 
 optional arguments:
   -h, --help            show this help message and exit
--- main.py
+++ main.py
@@ -6,6 +6,22 @@ import datetime
 import argparse
 import telegram
 import time
+import shutil
+import random
+
+
+def write_string_into_log(message):
+    with open('log.txt', 'a') as logfile:
+        time_stamp = datetime.datetime.now()
+        logfile.write(f'{time_stamp}. {message}\n')
+
+
+def get_exception(location, exception):
+    message = f'Exception of type {type(exception).__name__} occurred in {location}: {exception}'
+    with open('log.txt', 'a') as logfile:
+        time_stamp = datetime.datetime.now()
+        logfile.write(f'{time_stamp}. {message}\n')
+    print(message)
 
 
 def get_image(url, path):
@@ -15,6 +31,16 @@ def get_image(url, path):
         file.write(response.content)
 
 
+def get_random_flight_number():
+    flight_numbers = []
+    url = 'https://api.spacexdata.com/v3/launches'
+    response = requests.get(url)
+    for flight in response.json():
+        flight_numbers.append(flight['flight_number'])
+    random_flight_number = random.choice(flight_numbers)
+    return random_flight_number
+
+
 def get_spacex_links(launch_number):
     url = f'https://api.spacexdata.com/v3/launches/{launch_number}'
     response = requests.get(url)
@@ -22,25 +48,45 @@ def get_spacex_links(launch_number):
     return response.json()['links']['flickr_images']
 
 
-def fetch_spacex_last_launch(folder, launch_number):
-    spacex_folder = f'{folder}/spacex'
-    os.makedirs(spacex_folder, exist_ok=True)
+def check_image_quantity(folder, folder_number):
+    sub_folder = f'{folder}/{folder_number}'
+    os.makedirs(sub_folder, exist_ok=True)
+    if len(os.listdir(sub_folder)) == 9:
+        folder_number += 1
+    return sub_folder, folder_number
+
+
+def fetch_spacex_last_launch(settings, token):
+    spacex_folder = f"{settings['directory']}/spacex"
     filename = 'spacex'
-    links = get_spacex_links(launch_number)
+    flight_number = get_random_flight_number()
+    links = []
+    while len(links) == 0:
+        links = get_spacex_links(flight_number)
+    write_string_into_log(f'received {len(links)} spacex links')
+    folder_number = 0
+    num_of_link = 0
     for link_number, link in enumerate(links):
-        path = f'{spacex_folder}/{filename}{link_number}.jpg'
+        sub_folder, folder_number = check_image_quantity(
+            spacex_folder, folder_number)
+        path = f'{sub_folder}/{filename}{link_number}.jpg'
         get_image(link, path)
+        num_of_link += 1
+    write_string_into_log(f'{num_of_link} images has downloaded')
+    post_to_telegram_channel(
+        token, settings['telegram_chat_id'], spacex_folder)
 
 
-def get_nasa_apod_links(count, api_key):
+def get_nasa_apod_links(count_apod, api_key):
     url = 'https://api.nasa.gov/planetary/apod?'
-    params = {'count': count,
+    params = {'count': count_apod,
               'api_key': api_key}
     response = requests.get(url, params=params)
     response.raise_for_status()
     nasa_links = []
     for image_data in response.json():
-        nasa_links.append(image_data['url'])
+        if image_data['media_type'] == 'image':
+            nasa_links.append(image_data['url'])
     return nasa_links
 
 
@@ -53,15 +99,24 @@ def get_image_extension(url):
     return extension
 
 
-def fetch_nasa_apod(folder, api_key, count):
-    nasa_apod_folder = f'{folder}/nasa_apod'
-    os.makedirs(nasa_apod_folder, exist_ok=True)
+def fetch_nasa_apod(settings, api_key, token):
+    nasa_apod_folder = f"{settings['directory']}/nasa_apod"
     filename = 'nasa_apod'
-    links = get_nasa_apod_links(count, api_key)
+    links = get_nasa_apod_links(settings['image_quantity'], api_key)
+    write_string_into_log(f'received {len(links)} apod links')
+    folder_number = 0
+    num_of_link = 0
     for link_number, link in enumerate(links):
+        sub_folder, folder_number = check_image_quantity(
+            nasa_apod_folder, folder_number)
         image_extension = get_image_extension(link)
-        path = f'{nasa_apod_folder}/{filename}{link_number}{image_extension}'
-        get_image(link, path)
+        if image_extension != '':
+            path = f'{sub_folder}/{filename}{link_number}{image_extension}'
+            get_image(link, path)
+            num_of_link += 1
+    write_string_into_log(f'{num_of_link} images has downloaded')
+    post_to_telegram_channel(
+        token, settings['telegram_chat_id'], nasa_apod_folder)
 
 
 def combine_nasa_epic_link(data, api_key):
@@ -73,27 +128,36 @@ def combine_nasa_epic_link(data, api_key):
 
 
 def get_nasa_epic_links(api_key, days_ago):
-    request_data = datetime.date.today() - datetime.timedelta(days=days_ago)
+    request_data = datetime.date.today() - datetime.timedelta(days=int(days_ago))
     url = f'https://epic.gsfc.nasa.gov/api/natural/date/{request_data}'
     response = requests.get(url)
     response.raise_for_status()
     links = []
     for image_data in response.json():
-        date = datetime.datetime.fromisoformat(image_data['date'])
+        date, time = str(datetime.datetime.fromisoformat(image_data['date'])).split(sep=' ')
+        year, month, day = date.split(sep='-')
         link = combine_nasa_epic_link(
-            (image_data['image'], date.year, date.month, date.day), api_key)
+            (image_data['image'], year, month, day), api_key)
         links.append(link)
     return links
 
 
-def fetch_nasa_epic(folder, api_key, days_ago):
-    nasa_epic_folder = f'{folder}/nasa_epic'
-    os.makedirs(nasa_epic_folder, exist_ok=True)
+def fetch_nasa_epic(settings, api_key, token):
+    nasa_epic_folder = f"{settings['directory']}/nasa_epic"
     filename = 'nasa_epic'
-    links = get_nasa_epic_links(api_key, days_ago)
+    links = get_nasa_epic_links(api_key, settings['days_ago'])
+    write_string_into_log(f'received {len(links)} epic links')
+    folder_number = 0
+    num_of_link = 0
     for link_number, link in enumerate(links):
-        path = f'{nasa_epic_folder}/{filename}{link_number}.png'
+        sub_folder, folder_number = check_image_quantity(
+            nasa_epic_folder, folder_number)
+        path = f'{sub_folder}/{filename}{link_number}.png'
         get_image(link, path)
+        num_of_link += 1
+    write_string_into_log(f'{num_of_link} images has downloaded')
+    post_to_telegram_channel(
+        token, settings['telegram_chat_id'], nasa_epic_folder)
 
 
 def post_to_telegram_channel(token, chat_id, folder):
@@ -102,43 +166,67 @@ def post_to_telegram_channel(token, chat_id, folder):
         sub_folder_paths = f'{folder}/{sub_folder}'
         sub_folder_content = os.listdir(sub_folder_paths)
         media_group = []
-        print(len(media_group))
         for image in sub_folder_content:
-            if len(media_group) < 10:
-                image_path = f'{sub_folder_paths}/{image}'
-                media = telegram.files.inputmedia.InputMediaPhoto(media=open(image_path, 'rb'))
-                media_group.append(media)
-                print(len(media_group))
-        print(media_group)
+            image_path = f'{sub_folder_paths}/{image}'
+            media = telegram.files.inputmedia.InputMediaPhoto(
+                media=open(image_path, 'rb'))
+            media_group.append(media)
         bot.send_media_group(chat_id=chat_id, media=media_group)
-        time.sleep(10)
+        write_string_into_log(f'{folder}/{sub_folder} successfully uploaded')
+        time.sleep(60)
 
 
-def get_arguments_from_console():
+def get_arguments():
     parser = argparse.ArgumentParser(
-        description='Загрузите красивые фото космоса от SpaceX и NASA в Телеграм-канал')
+        description='Загрузка фото космоса от SpaceX и NASA в Телеграм-канал')
     parser.add_argument(
-        'telegram_chat_id', default="@BeautifulSpacePhoto", help='id Телеграм-канала')
+        '-l', '--launch_number',
+        help='Номер запуска SpaseX')
     parser.add_argument(
-        '-l', '--launch_number', default=16, help='Номер запуска SpaseX')
+        '-c', '--count', default=9,
+        help='Кол-во фотографий NASA APOD')
     parser.add_argument(
-        '-c', '--count', default=10, help='Кол-во фотографий NASA APOD')
-    parser.add_argument(
-        '-d', '--days_ago', default=8, help='Как давно сделаны фото NASA EPIC')
+        '-d', '--days_ago', default=8,
+        help='Как давно сделаны фото NASA EPIC')
     parser.add_argument(
         '-dir', '--directory', default='images',
         help='Путь к папке для скачанных картинок')
     args = parser.parse_args()
-    return args.telegram_chat_id, args.directory, args.launch_number, args.count, args.days_ago
+    args_dict = {'directory': args.directory,
+                 'launch_number': args.launch_number,
+                 'image_quantity': args.count,
+                 'days_ago': args.days_ago
+                 }
+    return args_dict
+
+
+def wait_before_nex_post(time_in_seconds=10):
+    time.sleep(time_in_seconds)
+    print(f'finish, next start in {time_in_seconds} seconds')
 
 
 if __name__ == '__main__':
-    telegram_chat_id, image_folder, launch_number, count_apod, epic_photo_days_ago = get_arguments_from_console()
+    arguments = get_arguments()
     load_dotenv()
     nasa_api_key = os.environ['NASA_TOKEN']
     telegram_token = os.environ['TELEGRAM_TOKEN']
-    fetch_spacex_last_launch(image_folder, launch_number)
-    fetch_nasa_apod(image_folder, nasa_api_key, count_apod)
-    fetch_nasa_epic(image_folder, nasa_api_key, epic_photo_days_ago)
-    post_to_telegram_channel(telegram_token, chat_id)
-    post_to_telegram_channel(telegram_token, telegram_chat_id, image_folder)
+    arguments['telegram_chat_id'] = os.environ['TELEGRAM_CHAT_ID']
+    delay = int(os.environ['DELAY'])
+    count = 1
+    while True:
+        print(f'Sending image pack #{count} at {datetime.datetime.now()}')
+        try:
+            fetch_spacex_last_launch(arguments, telegram_token)
+        except Exception as e:
+            get_exception('Spacex part', e)
+        try:
+            fetch_nasa_apod(arguments, nasa_api_key, telegram_token)
+        except Exception as e:
+            get_exception('Apod part', e)
+        try:
+            fetch_nasa_epic(arguments, nasa_api_key, telegram_token)
+        except Exception as e:
+            get_exception('Epic part', e)
+        shutil.rmtree(arguments['directory'])
+        wait_before_nex_post(delay)
+        count += 1
