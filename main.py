23f7ecf8e2d2149c2ea1d79255979cef63c34474
Andrey Sokoloff <smalldirtybird@gmail.com>
2021-11-24 16:38:00 +0300

    removed garbage code from main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import argparse
 import telegram
 import time
 import shutil
-from PIL import Image
 import logging
 import random
 
@@ -16,19 +15,6 @@ def get_file_size(filepath):
     size_in_bytes = os.path.getsize(filepath)
     size_in_megabytes = size_in_bytes / (1024 ** 2)
     return size_in_megabytes
-
-
-def big_file_compression(filepath, original_file_size, max_size):
-    coefficient = max_size / original_file_size
-    original_image = Image.open(filepath)
-    original_width, original_height = original_image.size
-    requirement_resolution = (
-        round(original_width * coefficient),
-        round(original_height * coefficient)
-        )
-    requirement_image = original_image.resize(
-        requirement_resolution, Image.ANTIALIAS)
-    requirement_image.save(filepath, optimize=True, quality=95)
 
 
 def get_image(url, path):
@@ -56,18 +42,9 @@ def get_spacex_links(launch_number):
     return response.json()['links']['flickr_images']
 
 
-def check_image_quantity(folder, folder_number):
-    sub_folder = os.path.join(folder, str(folder_number))
-    os.makedirs(sub_folder, exist_ok=True)
-    if len(os.listdir(sub_folder)) == 9:
-        folder_number += 1
-    return sub_folder, folder_number
-
-
 def fetch_spacex_last_launch(folder):
     filename = 'spacex'
     links = get_spacex_links(get_latest_flight_number())
-    logging.info(f'received {len(links)} spacex links')
     for link_number, link in enumerate(links):
         path = os.path.join(folder, f'{filename}{link_number}.jpg')
         get_image(link, path)
@@ -98,7 +75,6 @@ def get_image_extension(url):
 def fetch_nasa_apod(folder, image_quantity, api_key):
     filename = 'nasa_apod'
     links = get_nasa_apod_links(image_quantity, api_key)
-    logging.info(f'received {len(links)} apod links')
     for link_number, link in enumerate(links):
         image_extension = get_image_extension(link)
         if image_extension:
@@ -118,14 +94,14 @@ def combine_nasa_epic_link(data, api_key):
 def get_nasa_epic_links(api_key):
     days_ago = 0
     response_elements = []
-    while len(response_elements) == 0:
+    while not len(response_elements):
         request_data = datetime.date.today() - datetime.timedelta(
             days=int(days_ago))
         url = f'https://epic.gsfc.nasa.gov/api/natural/date/{request_data}'
         response = requests.get(url)
         response.raise_for_status()
         response_elements = [
-            elm for elm in response.json() if len(response.json()) != 0]
+            elm for elm in response.json() if len(response.json())]
         days_ago += 1
     links = []
     for image_data in response.json():
@@ -141,7 +117,6 @@ def get_nasa_epic_links(api_key):
 def fetch_nasa_epic(folder, api_key):
     filename = 'nasa_epic'
     links = get_nasa_epic_links(api_key)
-    logging.info(f'received {len(links)} epic links')
     for link_number, link in enumerate(links):
         path = os.path.join(folder, f'{filename}{link_number}.png')
         get_image(link, path)
@@ -190,28 +165,9 @@ if __name__ == '__main__':
         os.makedirs(image_folder, exist_ok=True)
         try:
             fetch_spacex_last_launch(image_folder)
-        except Exception:
-            logging.exception('fetch_spacex_last_launch')
-        try:
             fetch_nasa_apod(image_folder, apod_photo_count, nasa_api_key)
-        except Exception:
-            logging.exception('fetch_nasa_apod')
-        try:
             fetch_nasa_epic(image_folder, nasa_api_key)
-        except Exception:
-            logging.exception('fetch_nasa_epic')
+        except Exception as e:
+            logging.exception(e)
         post_to_telegram_channel(telegram_token, telegram_chat_id, image_folder, int(os.environ['DELAY']))
-        try:
-            shutil.rmtree(image_folder)
-        except Exception:
-            logging.exception('shutil.rmtree')
-        try:
-            time.sleep(int(os.environ['DELAY']))
-        except KeyError:
-            logging.exception(f'''
-                Variable "DELAY" not found in .env file.
-                Default time of delay between script running is 24 hours.
-                If need to set another delay value,
-                check README.md for instructions.
-                ''')
-            time.sleep(86200)
+        shutil.rmtree(image_folder)
